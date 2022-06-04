7c728def541894d69838b2c90fc9b68e9ef5a901
Carcamo-ben <carcamo.ben@gmail.com>
2022-06-04 00:17:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template
 app = Flask(__name__,template_folder='templates')
 
 @app.route("/")
-def hello():
+def index():
     return render_template("index.html")
 
 @app.route("/recetas")
-def hello():
+def recetas():
     return render_template("recetas.html")
 
