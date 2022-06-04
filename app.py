948caf820521412df9e4ca42dc81723e847f63bd
Carcamo-ben <carcamo.ben@gmail.com>
2022-06-03 20:58:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,3 @@ app = Flask(__name__,template_folder='templates')
 def hello():
     return render_template("index.html")
 
-@app.route("/recetas")
-def hello():
-    return render_template("recetas.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
