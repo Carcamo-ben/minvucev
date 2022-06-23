2099353327bbe2fe42b1afe904a4f5a29bf353a8
Benjamin Carcamo <bcarcamo@microsoft.com>
2022-06-23 03:19:48 -0400

    okay

--- app.py
+++ app.py
@@ -9,20 +9,8 @@ def index():
     if request.method == 'POST':
         username = request.form.get('comuna')  # access the data inside 
         password = request.form.get('latitud')
-    latitud = request.form.get('latitud')[0]
-    comuna = request.form.get('comuna') [0]
-    message=json.dumps(message)
+    message=json.dumps({"latitud":request.form.get('latitud'),"comuna":request.form.get('comuna')})
     print (message)
-    res = requests.post('https://prod-25.eastus.logic.azure.com:443/workflows/82bb213964e7477e97c1b949fe51544f/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=bDydfFCFFrBmgCF1qHpjCGIMVoKI_5CcmCSV1n2v5kw', data = {"latitud":latitud,"comuna":comuna} ,json = message)
+    res = requests.post('https://prod-25.eastus.logic.azure.com:443/workflows/82bb213964e7477e97c1b949fe51544f/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=bDydfFCFFrBmgCF1qHpjCGIMVoKI_5CcmCSV1n2v5kw', data = message, headers = {"Content-Type": "application/json"})
     print (res)
     return render_template('index.html', message=message)
-"""
-
-@app.route("/handle_form", methods = ['GET', 'POST', 'DELETE'])
-def handle_form():
-    data= request.form
-    print (data['comuna'])
-    print (data['latitud'])
-    request.post("https://prod-25.eastus.logic.azure.com:443/workflows/82bb213964e7477e97c1b949fe51544f/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=bDydfFCFFrBmgCF1qHpjCGIMVoKI_5CcmCSV1n2v5kw",data)
-    return data
-"""
