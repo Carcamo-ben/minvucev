cb49f4c28679d60c574b3b5702f4a428a2c8c021
Benjamin Carcamo <bcarcamo@microsoft.com>
2022-06-23 00:24:16 -0400

    hi

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for,request
 app = Flask(__name__,template_folder='templates')
 
 @app.route("/")
@@ -9,3 +9,9 @@ def index():
 def recetas():
     return render_template("recetas.html")
 
+@app.route('/handle_form>', methods = ['GET', 'POST', 'DELETE'])
+def handle_form:
+    data= request.form
+    print (data['comuna'])
+    print (data['latitud'])
+    return data
--- script.py
+++
@@ -1,6 +0,0 @@
-a=open("Rules.txt", "r",encoding='utf-8')
-b=open("Rulespy.txt","w",encoding='utf-8')
-for i in range (300):
-    s=a.readline()
-    if ":" in s or "*" in s:
-        b.write(s[5:len(s)-2]+"\n")
