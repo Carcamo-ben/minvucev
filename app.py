7dc8345f3944b03d5c07f8567f170b10e884698e
Benjamin Carcamo <bcarcamo@microsoft.com>
2022-06-23 01:46:52 -0400

    hi

--- app.py
+++ app.py
@@ -12,8 +12,10 @@ def index():
 
         if username == 'root' and password == 'pass':
             message = username,"Correct username and password",password
+            res=res
         else:
             message = username,"Wrong username or password",password
+            res=res
     message=json.dumps(message)
     print (message)
     res = requests.post('https://prod-25.eastus.logic.azure.com:443/workflows/82bb213964e7477e97c1b949fe51544f/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=bDydfFCFFrBmgCF1qHpjCGIMVoKI_5CcmCSV1n2v5kw', data = message)
