2eabdda8e1f084c215853e2c7ee6d873d7d134c7
Benjamin Carcamo <bcarcamo@microsoft.com>
2022-06-23 01:59:44 -0400

    hi

--- app.py
+++ app.py
@@ -9,16 +9,10 @@ def index():
     if request.method == 'POST':
         username = request.form.get('comuna')  # access the data inside 
         password = request.form.get('latitud')
-
-        if username == 'root' and password == 'pass':
-            message = username,"Correct username and password",password
-            res=res
-        else:
-            message = username,"Wrong username or password",password
-            res=res
     message=json.dumps(message)
     print (message)
     res = requests.post('https://prod-25.eastus.logic.azure.com:443/workflows/82bb213964e7477e97c1b949fe51544f/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=bDydfFCFFrBmgCF1qHpjCGIMVoKI_5CcmCSV1n2v5kw', data = message)
+    print (res)
     return render_template('index.html', message=message)
 """
 
