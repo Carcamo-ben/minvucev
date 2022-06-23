bbbd0b5c81720b0c6b3f748815dcbf640ab9c816
Benjamin Carcamo <bcarcamo@microsoft.com>
2022-06-23 01:08:22 -0400

    hi

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
-from flask import Flask, render_template, url_for,request
+from flask import Flask, render_template, url_for,request,json
+import requests
 app = Flask(__name__,template_folder='templates')
 
-@app.route('/index/', methods=['post', 'get'])
+@app.route('/', methods=['post', 'get'])
 def index():
     message = ''
     if request.method == 'POST':
-        username = request.form.get('username')  # access the data inside 
-        password = request.form.get('password')
+        username = request.form.get('comuna')  # access the data inside 
+        password = request.form.get('latitud')
 
         if username == 'root' and password == 'pass':
-            message = "Correct username and password"
+            message = username,"Correct username and password",password
         else:
-            message = "Wrong username or password"
-
+            message = username,"Wrong username or password",password
+    message=json.dumps(message)
+    print (message)
+    res = requests.post('https://prod-25.eastus.logic.azure.com:443/workflows/82bb213964e7477e97c1b949fe51544f/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=bDydfFCFFrBmgCF1qHpjCGIMVoKI_5CcmCSV1n2v5kw', data = message)
     return render_template('index.html', message=message)
 """
 
