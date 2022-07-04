4ae3936325fc0c4a29640c6e48fc61ddbc7eb9e9
Carcamo-ben <carcamo.ben@gmail.com>
2022-07-04 16:14:49 -0400

    Minvu Enviroment change url

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ def index():
     res = ''
     message=json.dumps({"latitud":request.form.get('latitud'),"comuna":request.form.get('comuna'),"zonatermica":request.form.get('zonatermica')})
     print (message)
-    res = requests.post('https://prod-25.eastus.logic.azure.com:443/workflows/82bb213964e7477e97c1b949fe51544f/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=bDydfFCFFrBmgCF1qHpjCGIMVoKI_5CcmCSV1n2v5kw', data = message, headers = {"Content-Type": "application/json"})
+    res = requests.post('https://prod-85.eastus.logic.azure.com:443/workflows/d240ea3b2d4c421eb793d86ed8a0663d/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=xHhaMjj22xzP71gyE4x3V5q8sMkZBXWuoquOwoVG6RE', data = message, headers = {"Content-Type": "application/json"})
     print (res)
     return render_template('index.html', message=message)
